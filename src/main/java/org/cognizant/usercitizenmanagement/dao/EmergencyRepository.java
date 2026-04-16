500d388ae1e8314dfef1ef276618d57d8794ab7f
k-mohan-cts <kommindala.mohan@cognizant.com>
2026-04-16 16:48:13 +0530

    update file strucutre with appropiate names

--- src/main/java/org/cognizant/usercitizenmanagement/dao/EmergencyRepository.java
+++
@@ -1,7 +0,0 @@
-package org.cognizant.usercitizenmanagement.dao;
-
-import org.cognizant.usercitizenmanagement.entity.EmergencyReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmergencyRepository extends JpaRepository<EmergencyReport, Integer> {
-}
